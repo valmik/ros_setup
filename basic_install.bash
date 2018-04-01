5f4344988db480987c7ef49b55c9e8aa9a3f52a9
valmik <rsnorunt@gmail.com>
2018-04-01 01:12:01 -0700

    add chrome install

--- basic_install.bash
+++ basic_install.bash
@@ -3,3 +3,7 @@
 # Install sublime
 chmod +x sublime_install.bash
 ./sublime_install.bash
+
+# Install chrome
+chmod +x chrome_install.bash
+./chrome_install.bash
---
+++ chrome_install.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://askubuntu.com/questions/79280/how-to-install-chrome-browser-properly-via-command-line
+
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
