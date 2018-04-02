d955f9b378bd588b21546c9e23ec543f16289526
valmik <rsnorunt@gmail.com>
2018-04-01 17:25:54 -0700

    kinova install

--- haptica_install.bash
+++ haptica_install.bash
@@ -2,7 +2,7 @@
 
 # Ros install
 chmod +x ros_install.bash
-./ros_install.bash
+# ./ros_install.bash
 
 echo "****************************** Installing Dependencies"
 # Install AR Track Alvar
@@ -28,6 +28,24 @@ source /opt/ros/kinetic/setup.bash
 mkdir -p ~/ros_workspaces/kinova_ws/src
 cd ~/ros_workspaces/kinova_ws/src
 catkin_init_workspace
+
+
+echo "****************************** Download Packages"
+
+
+# https://github.com/Kinovarobotics/kinova-ros/wiki/MoveIt
+# Kinova robotics package + trac-ik
+git clone -b kinetic_devel https://github.com/CNURobotics/kinova-ros.git
+sudo apt-get install ros-kinetic-trac-ik
+
+# Gazebo packages
+# https://github.com/Kinovarobotics/kinova-ros/wiki/Gazebo
+sudo apt-get install ros-kinetic-gazebo-ros-control
+sudo apt-get install ros-kinetic-ros-controllers
+
+
+
 cd ..
 catkin_make
 
+source devel/setup.bash
