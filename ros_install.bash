8a78c99b4a05d0511c4a05d78d0179f990485024
valmik <rsnorunt@gmail.com>
2020-08-06 17:40:30 -0700

    changed install to melodic

--- ros_install.bash
+++ ros_install.bash
@@ -1,35 +1,37 @@
 
-# First install ros-kinetic
-echo "****************************** Installing ROS Kinetic"
+# First install ros-melodic
+echo "****************************** Installing ROS Melodic"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt-get update
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt-get install ros-melodic-desktop-full
 
 sudo rosdep init
 rosdep update
 
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
-
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo rosdep init
+rosdep update
 
 # Install MoveIt
 echo "****************************** Installing MoveIt"
-sudo apt-get install ros-kinetic-moveit
+sudo apt-get install ros-melodic-moveit
 
+# Hopefully unneccesary
 # Update assimp to 3.3 (for moveit)
-echo "****************************** Updating assimp to v3.3"
-mkdir ~/temp
-cd ~/temp
-wget https://github.com/assimp/assimp/archive/v3.3.zip
-unzip v3.3.zip
-cd assimp-3.3
-cmake CMakeLists.txt -G 'Unix Makefiles'
-make
-sudo make install
-cd port/PyAssimp/
-sudo python setup.py install
+# echo "****************************** Updating assimp to v3.3"
+# mkdir ~/temp
+# cd ~/temp
+# wget https://github.com/assimp/assimp/archive/v3.3.zip
+# unzip v3.3.zip
+# cd assimp-3.3
+# cmake CMakeLists.txt -G 'Unix Makefiles'
+# make
+# sudo make install
+# cd port/PyAssimp/
+# sudo python setup.py install
 
